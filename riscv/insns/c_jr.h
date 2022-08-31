bce288d8e407a6e8266d0392245d985a5ad238e2
Alingof <Alignof@outlook.com>
2022-08-31 18:03:59 +0900

    [update] update debug msg

--- riscv/insns/c_jr.h
+++ riscv/insns/c_jr.h
@@ -7,6 +7,10 @@ if (insn.rvc_rs1() == 1) {
     uint64_t ret_addr = p->get_state()->shstk.pop();
     if (ret_addr != (RVC_RS1 & ~reg_t(1))) {
         // exception
-        fprintf(stderr, "exception: value popped from shstk does not match return addr\n");
+        fprintf(
+            stderr,
+            "exception at [0x%x]: value popped from shstk does not match return addr\n",
+            pc
+        );
     }
 }
--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -2,16 +2,20 @@ reg_t tmp = npc;
 set_pc((RS1 + insn.i_imm()) & ~reg_t(1));
 // call
 if (insn.rd() == 1 && insn.rs1() == 1) {
-    fprintf(stderr, "call exec\n");
+    fprintf(stderr, "call\n");
     p->get_state()->shstk.push(tmp);
 }
 // ret
 if (insn.rd() == 0 && insn.rs1() == 1 && insn.i_imm() == 0) {
-    fprintf(stderr, "ret exec\n");
+    fprintf(stderr, "ret\n");
     uint64_t ret_addr = p->get_state()->shstk.pop();
     if (ret_addr != ((RS1 + insn.i_imm()) & ~reg_t(1))) {
         // exception
-        fprintf(stderr, "exception: value popped from shstk does not match return addr\n");
+        fprintf(
+            stderr,
+            "exception at [0x%x]: value popped from shstk does not match return addr\n",
+            pc
+        );
     }
 }
 WRITE_RD(tmp);
