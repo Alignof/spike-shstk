f550bc5bbe805bf3cd256a1477eece048b5e95a5
Alingof <Alignof@outlook.com>
2022-08-31 15:38:59 +0900

    [update] modify to print msg when exception

--- riscv/insns/c_jalr.h
+++ riscv/insns/c_jalr.h
@@ -11,6 +11,7 @@ if (insn.rvc_rd() == 1 && insn.rvc_rs1() == 1 && insn.rvc_imm() == 0) {
     uint64_t ret_addr = p->get_state()->shstk.pop();
     if (ret_addr != (RVC_RS1 & ~reg_t(1))) {
         // exception
+        printf("exception: value poped from shstk does not match return addr\n");
     }
 }
 WRITE_REG(X_RA, tmp);
--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -9,6 +9,7 @@ if (insn.rd() == 1 && insn.rs1() == 1 && insn.i_imm() == 0) {
     uint64_t ret_addr = p->get_state()->shstk.pop();
     if (ret_addr != ((RS1 + insn.i_imm()) & ~reg_t(1))) {
         // exception
+        printf("exception: value poped from shstk does not match return addr\n");
     }
 }
 WRITE_RD(tmp);
