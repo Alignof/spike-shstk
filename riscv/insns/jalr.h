8f7e334090865c8b4404f1613adc2a5a5d728730
Alingof <Alignof@outlook.com>
2022-08-31 18:29:33 +0900

    [fix] fix condition of call inst

--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -1,7 +1,7 @@
 reg_t tmp = npc;
 set_pc((RS1 + insn.i_imm()) & ~reg_t(1));
 // call
-if (insn.rd() == 1 && insn.rs1() == 1) {
+if (insn.rd() == 1) {
     fprintf(stderr, "call\n");
     p->get_state()->shstk.push(tmp);
 }
